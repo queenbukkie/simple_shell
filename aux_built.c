3976595526a3f21f5aba66c9adeb33ec5e191aae
queenbukkie <akinwaleliz00@gmail.com>
2023-08-23 17:53:10 -0700

    builtin

---
+++ aux_built.c
@@ -0,0 +1,96 @@
+#include "shell.h"
+
+/**
+ * _myexit - Exit the shell or set exit status for the calling code.
+ * @info: Pointer to the information structure.
+ *
+ * Return: 1 if there's an error with argument or exit number,
+ *         -2 if an exit number is provided, or
+ *         -2 if no argument is provided (default exit).
+ */
+int _myexit(info_t *info)
+{
+	int exitcheck;
+
+	if (info->argv[1])  /* Check for exit argument */
+	{
+		exitcheck = _erratoi(info->argv[1]);
+		if (exitcheck == -1)
+		{
+			info->status = 2;
+			print_error(info, "Illegal number: ");
+			_eputs(info->argv[1]);
+			_eputchar('\n');
+			return (1);
+		}
+		info->err_num = _erratoi(info->argv[1]);
+		return (-2);
+	}
+	info->err_num = -1;
+	return (-2);
+}
+
+/**
+ * _mycd - Change the current directory of the process.
+ * @info: Pointer to the information structure.
+ *
+ * Return: Always 0.
+ */
+int _mycd(info_t *info)
+{
+	char *s, *dir, buffer[1024];
+	int chdir_ret;
+
+	s = getcwd(buffer, 1024);
+	if (!s)
+		_puts("TODO: >>getcwd failure emsg here<<\n");
+	if (!info->argv[1])
+	{
+		dir = _getenv(info, "HOME=");
+		if (!dir)
+			chdir_ret = /* TODO: Decide on default directory */;
+		else
+			chdir_ret = chdir(dir);
+	}
+	else if (_strcmp(info->argv[1], "-") == 0)
+	{
+		if (!_getenv(info, "OLDPWD="))
+		{
+			_puts(s);
+			_putchar('\n');
+			return (1);
+		}
+		_puts(_getenv(info, "OLDPWD=")), _putchar('\n');
+		chdir_ret = /* TODO: Decide on default directory */;
+	}
+	else
+		chdir_ret = chdir(info->argv[1]);
+	if (chdir_ret == -1)
+	{
+		print_error(info, "can't cd to ");
+		_eputs(info->argv[1]), _eputchar('\n');
+	}
+	else
+	{
+		_setenv(info, "OLDPWD", _getenv(info, "PWD="));
+		_setenv(info, "PWD", getcwd(buffer, 1024));
+	}
+	return (0);
+}
+
+/**
+ * _myhelp - Display information about built-in commands.
+ * @info: Pointer to the information structure.
+ *
+ * Return: Always 0.
+ */
+int _myhelp(info_t *info)
+{
+	char **arg_array;
+
+	arg_array = info->argv;
+	_puts("help call works. Function not yet implemented \n");
+	if (0)
+		_puts(arg_array); / Temporary unused workaround
+	return (0);
+}
